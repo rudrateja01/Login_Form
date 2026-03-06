5c89887a875e4563567fd84ae067b52cc799056a
P Rudra Teja Goud <rudrateja8897@gmail.com>
2026-03-06 14:04:02 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -15,12 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: "https://login-form-wjvu.onrender.com/",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
